529dc27d843761aa47f551234a972f10a36ef6f2
DHE <ydh.postkasten@yahoo.com>
2024-04-08 11:17:26 +0200

    dev

--- src/de/excample/ListExcamples.java
+++ src/de/excample/ListExcamples.java
@@ -1,10 +1,28 @@
 package de.excample;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListExcamples {
-    public static void main(String[] args) {
+    public static void main(String @NotNull [] args) {
+
         if (args.length < 1) {
-            System.out.printf("");
+            System.out.print("Zu wenig Argumente");
+            return;
+
 
         }
     }
+
+    public static void addAndRemoveElements() {
+        List<String> names = new ArrayList<String>();            // erzeuge eine Instanz mit der Variable names
+        // wir erzeugen hier eine List!!! aus der ArrayList!!!
+        //  das funktioniert weil alle methoden die im List steht stehen auch im nachfahren arraylist
+        
+
+    }
+
+
 }
